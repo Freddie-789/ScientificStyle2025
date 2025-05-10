e621d2d3bc4e2636548f2c155e341358a8f5762c
Freddie-789 <averageuser@mail.ru>
2025-05-10 12:29:24 +0700

    Create text_from_pdf.py
    Получает текст из скачанных pdf файлов

---
+++ text_from_pdf.py
@@ -0,0 +1,71 @@
+import os
+import re
+from pdfminer.high_level import extract_pages
+from pdfminer.layout import LTTextContainer, LTChar
+# LTTextContainer позволяет извлекать текст
+# LTChar шрифт и размер
+
+
+def text_extraction(element):
+    # Извлекаем текст из вложенного текстового элемента
+    line_text = element.get_text()
+    # Находим форматы текста
+    # инициализируем список со всеми форматами, встречающимися в строке текста
+    line_formats = []
+    for text_line in element:
+        # Проверяем, является ли элемент текстовым
+        if isinstance(text_line, LTTextContainer):
+            # Итеративно обходим каждый символ в строке текста
+            for character in text_line:
+                if isinstance(character, LTChar):
+                    # Добавляем к символу название шрифта
+                    line_formats.append(character.fontname)
+                    # Добавляем к символу размер шрифта
+                    line_formats.append(character.size)
+    # Находим уникальные размеры и названия шрифтов в строке
+    format_per_line = list(set(line_formats))
+    # Возвращаем кортеж с текстом в каждой строке вместе с его форматом
+    return line_text, format_per_line
+
+def get_text(file_path):
+    page_content = []
+    for page_number, page in enumerate(extract_pages(file_path)):
+        # Находим все элементы
+        page_elements = page._objs
+        # Находим элементы, составляющие страницу
+        for i, element in enumerate(page_elements):
+            # Проверяем, является ли элемент текстовым
+            if isinstance(element, LTTextContainer):
+                # Используем функцию извлечения текста и формата для каждого текстового элемента
+                (line_text, format_per_line) = text_extraction(element)
+                #print(line_text, format_per_line)
+                if (10.019999999999982 in format_per_line or 10.980000000000018 in format_per_line) and len(line_text.split()) > 3:
+                    line_text = re.sub(r'\[.*?\]', '', line_text)
+                    page_content.append(line_text)
+                if "Список литературы" in line_text:
+                    text1 = ""
+                    text2 = ""
+                    for text in page_content:
+                        split_text1 = text.split("-\n")
+                        clean_text1 = "".join(split_text1)
+                        text1 += clean_text1
+                    split_text2 = text1.split("\n")
+                    clean_text2 = "".join(split_text2)
+                    text2 += clean_text2
+                    return text2
+    return page_content
+
+#os.makedirs("C:\project2025\ScientificStyle\Texts")
+
+def list_of_texts(path):
+    file_list = os.listdir(path)[:4]
+    #print(file_list)
+    for num, file in enumerate(file_list):
+        folder_path = "C:\project2025\ScientificStyle\Texts"
+        file_path = os.path.join(folder_path, "scientific " + str(num) + ".txt")
+        with open(file_path, "w", encoding='utf-8') as f:
+            with open('C:\project2025\ScientificStyle\Articles/' + file, 'rb') as x:
+                a = get_text(x)
+                f.write(a)
+
+list_of_texts("C:\project2025\ScientificStyle\Articles")
